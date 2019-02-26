5f82af3e33300e5e5b5ca416bfe1c2d02d76bb35
Ching Ching Huang <chingchinghuang@n225-8.mtholyoke.edu>
2019-02-25 19:09:13 -0500

    Ching Direct Message Step 1 to 4

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -77,10 +77,12 @@ public class MessageServlet extends HttpServlet {
 
     String user = userService.getCurrentUser().getEmail();
     String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+    //get the value of query parameter
+    String recipient = request.getParameter("recipient");
 
-    Message message = new Message(user, text);
+    Message message = new Message(user, text,recipient);
     datastore.storeMessage(message);
 
-    response.sendRedirect("/user-page.html?user=" + user);
+    response.sendRedirect("/user-page.html?user=" + recipient);
   }
 }
