d8a898ea921ced69a52237fc235d9092f92c49f9
Ayesha Gagguturi <ag946@cornell.edu>
2019-03-04 15:41:36 -0500

    formatting fixes based on pull request. Including details on User Class and what it's intended function is.

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -100,9 +100,7 @@ function fetchAboutMe() {
     if (aboutMe == '') {
       aboutMe = 'This user has not entered any information yet.';
     }
-
     aboutMeContainer.innerHTML = aboutMe;
-
   });
 }
 
@@ -110,7 +108,6 @@ function fetchAboutMe() {
 /** Fetches data and populates the UI of the page. */
 function buildUI() {
   setPageTitle();
-
   showMessageFormIfViewingSelf();
   fetchMessages();
   fetchAboutMe();
