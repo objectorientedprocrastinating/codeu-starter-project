b36be82723cc44330cd8d3af870c12a8517afcf3
Ayesha Gagguturi <ag946@cornell.edu>
2019-03-06 13:59:18 -0500

    fixed the javadoc comments and logic for the case if email and about me strings are null.

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -1,31 +1,30 @@
 package com.google.codeu.data;
 
-//Provides the capability to save the data the user submits, 
-//and then retrieve that data to display the user's "about me" 
-//section on their user page.
+import java.util.Objects;
 
+/**
+ * Provides the capability to save the data the user submits, and then retrieve
+ * that data to display the user's "about me" section on their user page.
+ */
 public class User {
 
-    private String email;
-    private String aboutMe;
+  private String email;
+  private String aboutMe;
 
-    //If null is passed in through the method then return empty string for each field. 
-    public User(String email, String aboutMe) {
-      if(email == null || aboutMe == null){
-        this.email = "";
-        this.aboutMe = "";
-      }  
-      else{
-        this.email = email;
-        this.aboutMe = aboutMe;
-      }
-    }
+  /**
+   * If null is passed in through the method then return empty string for each
+   * field.
+   */
+  public User(String email, String aboutMe) {
+    this.email = Objects.toString(email, "");
+    this.aboutMe = Objects.toString(aboutMe, "");
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getAboutMe() {
-        return aboutMe;
-    }
+  public String getAboutMe() {
+    return aboutMe;
+  }
 }
