3e0c1107a971d64fbe02500bb03812a4a7e4ca2a
Ayesha Gagguturi <ag946@cornell.edu>
2019-03-06 16:28:31 -0500

    formatting for google style

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -3,8 +3,8 @@ package com.google.codeu.data;
 import java.util.Objects;
 
 /**
- * Provides the capability to save the data the user submits, and then retrieve
- * that data to display the user's "about me" section on their user page.
+ * Provides the capability to save the data the user submits, and then retrieve that data to display
+ * the user's "about me" section on their user page.
  */
 public class User {
 
@@ -12,8 +12,7 @@ public class User {
   private String aboutMe;
 
   /**
-   * If null is passed in through the method then return empty string for each
-   * field.
+   * If null is passed in through the method then return empty string for each field.
    */
   public User(String email, String aboutMe) {
     this.email = Objects.toString(email, "");
--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -1,12 +1,10 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
@@ -20,52 +18,52 @@ import org.jsoup.safety.Whitelist;
 @WebServlet("/about")
 public class AboutMeServlet extends HttpServlet {
 
-    private Datastore datastore;
+  private Datastore datastore;
 
-    @Override
-    public void init() {
-        datastore = new Datastore();
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  /**
+   * Responds with the "about me" section for a particular user.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    response.setContentType("text/html");
+
+    String user = request.getParameter("user");
+
+    if (user == null || user.equals("")) {
+      // Request is invalid, return empty response
+      return;
     }
 
-    /**
-     * Responds with the "about me" section for a particular user.
-     */
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    User userData = datastore.getUser(user);
 
-        response.setContentType("text/html");
-
-        String user = request.getParameter("user");
-
-        if (user == null || user.equals("")) {
-            // Request is invalid, return empty response
-            return;
-        }
-
-        User userData = datastore.getUser(user);
-
-        if (userData == null || userData.getAboutMe() == null) {
-            return;
-        }
-
-        response.getOutputStream().println(userData.getAboutMe());
+    if (userData == null || userData.getAboutMe() == null) {
+      return;
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.getOutputStream().println(userData.getAboutMe());
+  }
 
-        UserService userService = UserServiceFactory.getUserService();
-        if (!userService.isUserLoggedIn()) {
-            response.sendRedirect("/index.html");
-            return;
-        }
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        String userEmail = userService.getCurrentUser().getEmail();
-        String aboutMe = Jsoup.clean(request.getParameter("about-me"), Whitelist.none());
-
-        User user = new User(userEmail, aboutMe);
-        datastore.storeUser(user);
-
-        response.sendRedirect("/user-page.html?user=" + userEmail);
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
     }
+
+    String userEmail = userService.getCurrentUser().getEmail();
+    String aboutMe = Jsoup.clean(request.getParameter("about-me"), Whitelist.none());
+
+    User user = new User(userEmail, aboutMe);
+    datastore.storeUser(user);
+
+    response.sendRedirect("/user-page.html?user=" + userEmail);
+  }
 }
