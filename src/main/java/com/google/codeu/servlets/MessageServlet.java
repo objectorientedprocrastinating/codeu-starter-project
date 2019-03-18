92e3c0db4e5fdf2ac217de175a8cf90c3889653b
Ayesha Gagguturi <ag946@cornell.edu>
2019-03-18 00:53:45 -0400

    style changes for formatter

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -27,11 +27,11 @@ import com.google.codeu.data.Message;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
