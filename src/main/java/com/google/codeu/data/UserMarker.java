0620da80ca33cb9affb01672cdf732c50b7ad121
chingchinghuang <huang27c@mtholyoke.edu>
2019-04-02 10:35:54 -0400

    Add javadoc

--- src/main/java/com/google/codeu/data/UserMarker.java
+++ src/main/java/com/google/codeu/data/UserMarker.java
@@ -6,20 +6,42 @@ public class UserMarker {
   private double lng;
   private String content;
 
+  /**
+   * Create a usermarker.
+   *
+   * @param lat lattitude of the location.
+   * @param lng longitude of the location.
+   * @param content content to show in the info box.
+   */
   public UserMarker(double lat, double lng, String content) {
     this.lat = lat;
     this.lng = lng;
     this.content = content;
   }
 
+  /**
+   * Returns the lattitude.
+   *
+   * @return lat
+   */
   public double getLat() {
     return lat;
   }
 
+  /**
+   * Returns the longitude.
+   *
+   * @return lng
+   */
   public double getLng() {
     return lng;
   }
 
+  /**
+   * Returns the content.
+   *
+   * @return content
+   */
   public String getContent() {
     return content;
   }
