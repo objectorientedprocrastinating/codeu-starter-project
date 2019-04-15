3880292155ddb55b76377863d640075b858e34f1
chingchinghuang <huang27c@mtholyoke.edu>
2019-04-15 16:01:02 -0400

    Fix lint error

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ /* global google */
 
 // Get ?user=XYZ parameter value
-/* global google */
 const urlParams = new URLSearchParams(window.location.search);
 const parameterUsername = urlParams.get('user');
 let editMarker;
